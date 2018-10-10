5a954eb90df23d6819513bcac9c170c7b2d0c831
Ryan Stillings <rystills@gmail.com>
2018-10-10 19:29:23 -0400

    more DH

--- main.py
+++ main.py
@@ -3,20 +3,38 @@ Created on Oct 10, 2018
 
 @author: Ryan
 '''
-import numpy,random
+from numpy import poly1d
+from numpy.polynomial.polynomial import polypow
+from numpy import polydiv
+import random
+
+def GF(res):
+    print(res)
+    return poly1d([abs(res[i]%2) for i in range(len(res),-1,-1)])
 
 def main():
     #hard-coded constants (you can change these if you want)
-    primPoly = numpy.poly1d([1,0,0,0,0,0,1,1])
+    primPoly = poly1d([1,0,0,0,0,0,1,1])
+    G = 100
     
     #randomly choose A and B -> construct polynomials
-    Arand = random.randint(10,80); Brand = random.randint(10,60)
-    A = numpy.poly1d([1]+[0]*Arand); B = numpy.poly1d([1]+[0]*Brand)
+    #Arand = random.randint(1,G); Brand = random.randint(1,G)
+    Arand = 56
+    Brand = 18
+    A = poly1d([1]+[0]*Arand)
+    B = poly1d([1]+[0]*Brand)
     
     #apply modulo -> map to Galois field GF(2)
-    GF = lambda res: numpy.poly1d([abs(res[i]%2) for i in range(len(res),-1,-1)]) 
-    Ya = GF(numpy.polydiv(A,primPoly)[1]); Yb = GF(numpy.polydiv(B,primPoly)[1]);
-    print(Ya)
+    Ya = GF(polydiv(A,primPoly)[1])
+    Yb = GF(polydiv(B,primPoly)[1])
+    
+    #print(Ya,'\n',Yb)
+    
+    #exchange Ya and Yb : apply modulo -> map to Galois field GF(2)
+    Yba = GF(polydiv(poly1d(polypow(Yb,Arand)),primPoly)[1])
+    Yab = GF(polydiv(poly1d(polypow(Ya,Brand)),primPoly)[1])
+    
+    print(Yba,'\n',Yab)
     
 if __name__ == "__main__":
     main()
