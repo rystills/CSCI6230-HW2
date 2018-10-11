d03762d18a6b9f6eef4f520136f2f1d60232a01a
Ryan Stillings <rystills@gmail.com>
2018-10-10 22:58:11 -0400

    final 10-bit key calculation for diffie hellman

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-import sympy
-from numpy import array as nparray
-from numpy import dtype
-from numpy import poly1d
-from numpy.polynomial.polynomial import polypow
-from numpy import polydiv
-import random
-
+import sympy, random
 x = sympy.Symbol('x')
 
+"""
+map the polynomial res to GF(2)
+@param res: the polynomial to map to GF(2)
+returns the polynomial res mapped to GF(2)
+"""
 def GF(res):
     coeffs = res.all_coeffs()
     return sympy.Poly.from_list([abs(coeffs[i]%2) for i in range(len(coeffs))],gens=x)
     
-
 def main():
     #hard-coded constants (you can change these if you want)
-    primPoly = sympy.Poly.from_list([1,0,0,0,0,0,1,1],gens=x)
-
+    primPoly = sympy.Poly.from_list([1,0,0,0,0,0,0,0,0,1,1],gens=x)
     G = 100
     
     #randomly choose A and B -> construct polynomials
-    #Arand = random.randint(1,G); Brand = random.randint(1,G)
-    Arand = 56
-    Brand = 18
+    Arand = random.randint(1,G)
+    Brand = random.randint(1,G)
+    
     A = sympy.Poly.from_list([1]+[0]*Arand,gens=x)
     B = sympy.Poly.from_list([1]+[0]*Brand,gens=x)
     
@@ -30,13 +26,16 @@ def main():
     Ya = GF((sympy.div(A,primPoly,domain='QQ')[1]))
     Yb = GF((sympy.div(B,primPoly,domain='QQ')[1]))
     
-    print(Ya,'\n',Yb)
-    
     #exchange Ya and Yb : apply modulo -> map to Galois field GF(2)
     Yba = GF(sympy.div(GF(Yb**Arand),primPoly,domain='QQ')[1])
     Yab = GF(sympy.div(GF(Ya**Brand),primPoly,domain='QQ')[1])
     
-    print(Yba,'\n',Yab)
+    #convert poly to key 
+    key = Yab.all_coeffs()
+    key = [0] * (10-len(key)) + key
+    
+    #final verification
+    print("Ya:  {0}\nYb:  {1}\nYba: {2}\nYab: {3}\nkey: {4}".format(Ya,Yb,Yba,Yab,key))
     
 if __name__ == "__main__":
     main()
